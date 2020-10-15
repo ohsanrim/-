b4365be9cae9fccc815c4a7e0fdeb1231cb5f4ed
OHrin <ka28@naver.com>
2020-10-16 02:20:31 +0900

    [포트폴리오] 2020/10/16 1.work2 페이지 전면 수정 사진 및 정보 입력 스크롤 시 애니메이션 수정 2. about 페이지 수정 사진 hover 시 클릭할 수 있게 효과 넣기 사진 클릭 시 모달창으로 간단한 infomation 창이 뜨게 구현 타이틀 타이핑 기법을 사용하여 애니메이션 효과를 줌 3. 홈페이지 main 화면 csshake 효과 적용

--- js/portfolio.js
+++ js/portfolio.js
@@ -95,9 +95,15 @@ $("document").ready(function(){
         pos=height * (count);
         scrollevent(pos);
         //divevent 실행
-        if(count==1) divEvent(1);
+        if(count==1) {
+          typingStart();
+          divEvent(1);
+        }
         if(count==3) {
           slideLeft();
+        }
+        if(count==4){
+          slideRight();
         }
     })
 
@@ -151,6 +157,13 @@ $("document").ready(function(){
         if(e.keyCode==27){
             close_guide();
         }
+        if(count==1) divEvent(1);
+        if(count==3) {
+          slideLeft();
+        }
+        if(count==4){
+          slideRight();
+        }
     })
 
     //TOUCH SWIPE EVENT
@@ -165,8 +178,6 @@ $("document").ready(function(){
                 count--;
                 if(count<0) count=0;
             }
-
-
 
             //wheel count 값에 따른 scrollTop 지정
             $("html, body").stop().animate({
@@ -204,6 +215,13 @@ $("document").ready(function(){
             wc=0;
             $(".wrap .web .web_wrap .mockup li").removeClass("select")
             $(".wrap .web .web_wrap .txt li").hide().eq(0).show()
+        }
+        if(count==1) divEvent(1);
+        if(count==3) {
+          slideLeft();
+        }
+        if(count==4){
+          slideRight();
         }
     })
 
@@ -314,15 +332,65 @@ $("document").ready(function(){
      }
      $(document).ready(function(){
        $("#slideLeft").css('opacity','0');
+       $("#slideRight").css('opacity','0');
        $("#scrollEvent1").hide();
      });
      //사용자 애니메이션
      function slideLeft(){
        $("#slideLeft").animate({
           marginLeft: "100px",
-          marginRight: "0px",
+          marginRight: "100px",
           opacity: 1
         },
         2000);
      }
+     function slideRight(){
+       $("#slideRight").animate({
+          marginLeft: "100px",
+          marginRight: "100px",
+          opacity: 1
+        },
+        2000);
+     }
+     //타이핑 기법
+     var typingEnd=false;
+     function typingStart(){
+       if(!typingEnd){
+         var typingBool = false;
+        var typingIdx=0;
+        var typingTxt = $("#typing-txt").val(); // 타이핑될 텍스트를 가져온다
+        typingTxt=typingTxt.split(""); // 한글자씩 자른다.
+        if(typingBool==false){ // 타이핑이 진행되지 않았다면
+           typingBool=true;
+
+           var tyInt = setInterval(typing,110); // 반복동작
+         }
+
+         function typing(){
+           if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복
+             $("#typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다.
+             typingIdx++;
+           } else{
+             clearInterval(tyInt); //끝나면 반복종료
+             typingEnd=true;
+           }
+         }
+       }
+     }
+
+
+     //모달창css
+     $(document).ready(function(){
+       $('figure').click(function(){
+         $('.modal-wrapper').toggleClass('open');
+        $('.page-wrapper').toggleClass('blur-it');
+         return false;
+       });
+       $('.trigger').on('click', function() {
+          $('.modal-wrapper').toggleClass('open');
+         $('.page-wrapper').toggleClass('blur-it');
+          return false;
+       });
+     });
+
 })
